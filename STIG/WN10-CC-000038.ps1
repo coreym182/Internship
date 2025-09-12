8bba18391d65516b417fdd9def5e0747d69756cb
Corey Mitchell <135177708+coreym182@users.noreply.github.com>
2025-09-12 12:11:34 -0700

    Update WN10-CC-000038.ps1

--- STIG/WN10-CC-000038.ps1
+++ STIG/WN10-CC-000038.ps1
@@ -5,15 +5,15 @@ WN10-CC-000038
     This PowerShell script ensures that the maximum size of the Windows Application event log is at least 32768 KB (32 MB).
 
 .NOTES
-    Author          : Josh Madakor
-    LinkedIn        : linkedin.com/in/joshmadakor/
-    GitHub          : github.com/joshmadakor1
+    Author          : Corey Mitchell
+    LinkedIn        : www.linkedin.com/in/corey-mitchell182/
+    GitHub          : https://github.com/coreym182/Internship
     Date Created    : 2024-09-09
     Last Modified   : 2024-09-09
     Version         : 1.0
     CVEs            : N/A
     Plugin IDs      : N/A
-    STIG-ID         : WN10-AU-000500
+    STIG-ID         : WN10-CC-000038
 
 .TESTED ON
     Date(s) Tested  : 
