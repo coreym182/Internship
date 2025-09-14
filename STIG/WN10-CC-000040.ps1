9b786277913a970b0604ce8a79f423693ada5cc2
Corey Mitchell <135177708+coreym182@users.noreply.github.com>
2025-09-14 07:57:41 -0700

    Update WN10-CC-000040.ps1

--- STIG/WN10-CC-000040.ps1
+++ STIG/WN10-CC-000040.ps1
@@ -7,7 +7,7 @@ WN10-CC-000040
 
 <#
 .SYNOPSIS
-    This PowerShell script ensures that the maximum size of the Windows Application event log is at least 32768 KB (32 MB).
+    This PowerShell script ensures that Insecure logons to an SMB server must be disabled.
 
 .NOTES
     Author          : Corey Mitchell
